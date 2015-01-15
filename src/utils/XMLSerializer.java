719f855533c4f3e66ac40f7845809459c09335c7
Luke Halley <luke123halley@gmail.com>
2015-01-15 14:36:35 +1000

    Clarify XML serialization format for persistence

--- src/utils/XMLSerializer.java
+++ src/utils/XMLSerializer.java
@@ -7,6 +7,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+// Converts heap structure to XML for data persistence and recovery
 import java.util.Stack;
 
 import com.thoughtworks.xstream.XStream;
