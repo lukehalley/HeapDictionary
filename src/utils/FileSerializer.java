de112307e68a54a8443617ee8d7ef8b8b0bbd8a9
Luke Halley <luke123halley@gmail.com>
2015-08-28 10:17:09 +1000

    Add comment about file I/O handling

--- src/utils/FileSerializer.java
+++ src/utils/FileSerializer.java
@@ -12,6 +12,7 @@ public class FileSerializer {
 
 	public static void serializeFiles(ArrayList<Pair> dictionaryWords) {
 		XMLSerializer xs = new XMLSerializer();
+// Ensure proper file stream closure on completion
 // TODO: Implement comprehensive error handling for file I/O operations
 		
 		xs.push(dictionaryWords);
