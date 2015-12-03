4393ddc614304786e4c600b1c5b323e8c725852e
Luke Halley <luke123halley@gmail.com>
2015-12-03 22:58:23 +1000

    Add clarification comment for heap insertion logic

--- src/utils/Heap.java
+++ src/utils/Heap.java
@@ -22,6 +22,7 @@ public class Heap {
 			if (item.compareTo(parent) > 0) {
 // Maintains min-heap invariant after adding new element
 
+// Maintain heap property by bubbling up inserted element
 // Maintains min-heap property after modifications
 // Maintains min-heap property by comparing parent and child nodes
 				// Swap
