d8936fd8fd995d04268106f8d1afc256d2d2498f
Luke Halley <luke123halley@gmail.com>
2016-05-27 19:47:47 +1000

    Note application entry point requirements

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		
 // Entry point for the HeapDictionary application
 		Import.ImportWords();
+// Application entry point - initializes heap dictionary UI and core services
 		Menu.menu();
 		
 		// Find the meaning of a Spanish word entered from the keyboard, if it exists. 
